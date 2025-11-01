0bf4f384211fc4747ca110a14d5cedbb41b98bb7
Dikki AP <dikki.1710@gmail.com>
2025-11-01 20:43:21 +0700

    Initial

---
+++ src/main/java/dikki_dev/learn_spring_config_properties/LearnSpringConfigPropertiesApplication.java
@@ -0,0 +1,13 @@
+package dikki_dev.learn_spring_config_properties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringConfigPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringConfigPropertiesApplication.class, args);
+	}
+
+}
